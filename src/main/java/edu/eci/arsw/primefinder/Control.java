c5412d50c3c7b1ad4d371654c228a0e902ceeb79
carljob <uribe.vargas.carlos@gmail.com>
2026-06-03 10:14:55 -0500

    feat: implementar pausa y reanudación con wait/notifyAll en PrimeFinder

--- src/main/java/edu/eci/arsw/primefinder/Control.java
+++ src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -1,44 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.arsw.primefinder;
 
-/**
- *
- */
+import java.util.Scanner;
+
 public class Control extends Thread {
-    
+
     private final static int NTHREADS = 3;
     private final static int MAXVALUE = 30000000;
     private final static int TMILISECONDS = 5000;
 
     private final int NDATA = MAXVALUE / NTHREADS;
-
     private PrimeFinderThread pft[];
-    
+    private boolean paused = false;
+
     private Control() {
         super();
-        this.pft = new  PrimeFinderThread[NTHREADS];
-
+        this.pft = new PrimeFinderThread[NTHREADS];
         int i;
-        for(i = 0;i < NTHREADS - 1; i++) {
-            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA);
-            pft[i] = elem;
+        for (i = 0; i < NTHREADS - 1; i++) {
+            pft[i] = new PrimeFinderThread(i * NDATA, (i + 1) * NDATA, this);
         }
-        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1);
+        pft[i] = new PrimeFinderThread(i * NDATA, MAXVALUE + 1, this);
     }
-    
+
     public static Control newControl() {
         return new Control();
     }
 
     @Override
     public void run() {
-        for(int i = 0;i < NTHREADS;i++ ) {
+        for (int i = 0; i < NTHREADS; i++) {
             pft[i].start();
         }
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                Thread.sleep(TMILISECONDS);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+
+            synchronized (this) {
+                paused = true;
+            }
+
+            int total = 0;
+            for (PrimeFinderThread t : pft) {
+                total += t.getPrimes().size();
+            }
+            System.out.println("Primos encontrados hasta ahora: " + total);
+            System.out.println("Presiona ENTER para continuar...");
+            scanner.nextLine();
+
+            synchronized (this) {
+                paused = false;
+                notifyAll();
+            }
+        }
     }
-    
+
+    public synchronized void checkPause() {
+        while (paused) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
 }
